2c1cf8c79005fe8d980191e4aa2885175d7e04e3
pdevyatov <p.devyatov@gmail.com>
2020-06-16 11:28:37 +0300

    First commit

---
+++ conversionTracker.js
@@ -0,0 +1,25 @@
+function conversionTracker(buttonsClass, conversionName) {
+
+    const conversionButtons = document.getElementsByClassName(buttonsClass)
+
+    if (conversionButtons.length) {
+        for (let i = 0; i < conversionButtons.length; i++) {
+            conversionButtons[i].addEventListener("mousedown", function () {
+
+                // Yandex
+                ym(00000000, 'reachGoal', conversionName)
+
+                // Google
+                gtag('event', conversionName, { 'event_category': conversionName })
+
+                // Facebook
+                fbq('track', 'Lead')
+
+                // Print
+                console.log('Conversion "' + conversionName + '" from "' + buttonsClass + '" sent')
+
+            })
+        }
+    }
+
+}
